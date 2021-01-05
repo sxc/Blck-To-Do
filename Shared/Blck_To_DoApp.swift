f3063cde28fbc7c4f20ab437d64aaa0b0c7474ba
sxc <qiuyequ@icloud.com>
2021-01-05 14:24:17 +0800

    Initial Commit

---
+++ Shared/Blck_To_DoApp.swift
@@ -0,0 +1,20 @@
+//
+//  Blck_To_DoApp.swift
+//  Shared
+//
+//  Created by Xiaochun Shen on 2021/1/5.
+//
+
+import SwiftUI
+
+@main
+struct Blck_To_DoApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
